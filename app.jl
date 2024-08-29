df4b1119be0f328cbb124c4d7adc9254436e7516
PGimenez <gimenezfebrer@gmail.com>
2024-08-29 23:10:12 +0200

    demo

--- app.jl
+++ app.jl
@@ -15,58 +15,58 @@ Stipple.render(X::Matrix) = [X[:, i] for i in 1:size(X, 2)]
     # @out variables can only be modified by the backend
     # @in variables can be modified by both the backend and the browser
     # variables must be initialized with constant values, or variables defined outside of the @app block
-    @in N = 10
-    @in selected_cell = [0,0]
-    @out wafer_data = rand(10,10)
-    @out wafer_angle = 2.0*ones(10,10)
-    @in angle = 0.0
-    @in magnitude = 0.1
-    @in random_angle = false
-    @in animate = false
+    # @in N = 10
+    # @in selected_cell = [0,0]
+    # @out wafer_data = rand(10,10)
+    # @out wafer_angle = 2.0*ones(10,10)
+    # @in angle = 0.0
+    # @in magnitude = 0.1
+    # @in random_angle = false
+    # @in animate = false
     # file saving and loading
-    @in save = false
-    @in filename = "data"
-    @in selected_file = "" 
-    @out data_files = string.(unique([ split(f, "_")[1] for f in readdir("data")]))
+    # @in save = false
+    # @in filename = "data"
+    # @in selected_file = "" 
+    # @out data_files = string.(unique([ split(f, "_")[1] for f in readdir("data")]))
 
     # == Reactive handlers ==
     # reactive handlers watch a variable and execute a block of code when
     # its value changes
-    @onchange N begin
-        wafer_data = magnitude*rand(N,N)
-        wafer_angle = angle*ones(N,N)
-    end
-    @onchange magnitude begin
-        wafer_data = magnitude * rand(N,N)
-    end
-    @onchange angle begin
-        wafer_angle = angle*ones(N,N)
-    end
-    @onbutton random_angle begin
-        wafer_angle = 2.0*rand(N,N)
-    end
+    # @onchange N begin
+    #     wafer_data = magnitude*rand(N,N)
+    #     wafer_angle = angle*ones(N,N)
+    # end
+    # @onchange magnitude begin
+    #     wafer_data = magnitude * rand(N,N)
+    # end
+    # @onchange angle begin
+    #     wafer_angle = angle*ones(N,N)
+    # end
+    # @onbutton random_angle begin
+    #     wafer_angle = 2.0*rand(N,N)
+    # end
     # the onbutton handler will set the variable to false after the block is executed
-    @onbutton animate begin
-        angle = 0.0
-        magnitude = 0
-        for N in 10:20
-            angle += 0.6
-            magnitude += 0.3
-            sleep(0.5)
-        end
-    end
+    # @onbutton animate begin
+    #     angle = 0.0
+    #     magnitude = 0
+    #     for N in 10:20
+    #         angle += 0.6
+    #         magnitude += 0.3
+    #         sleep(0.5)
+    #     end
+    # end
 
-    @onbutton save begin
-        DelimitedFiles.writedlm("data/$(filename)_mag.csv", wafer_data, ",")
-        DelimitedFiles.writedlm("data/$(filename)_angle.csv", wafer_angle, ",")
-        data_files = string.(unique([ split(f, "_")[1] for f in readdir("data")]))
-        @show data_files
-    end
+    # @onbutton save begin
+    #     DelimitedFiles.writedlm("data/$(filename)_mag.csv", wafer_data, ",")
+    #     DelimitedFiles.writedlm("data/$(filename)_angle.csv", wafer_angle, ",")
+    #     data_files = string.(unique([ split(f, "_")[1] for f in readdir("data")]))
+    #     @show data_files
+    # end
 
-    @onchange selected_file begin
-        wafer_data = DelimitedFiles.readdlm("data/$(selected_file)_mag.csv", ',' , Float64)
-        wafer_angle = DelimitedFiles.readdlm("data/$(selected_file)_angle.csv", ',' , Float64)
-    end
+    # @onchange selected_file begin
+    #     wafer_data = DelimitedFiles.readdlm("data/$(selected_file)_mag.csv", ',' , Float64)
+    #     wafer_angle = DelimitedFiles.readdlm("data/$(selected_file)_angle.csv", ',' , Float64)
+    # end
 
     
 end
